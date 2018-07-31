273276ed039039f1027f0f79e936fbb38a243a57
tdeep17 <tdeep17@email.com>
2018-07-31 12:38:19 +0530

    Updated TestBase.Java, Config.Properties

--- PHPTravels/src/main/java/com/qa/travels/base/TestBase.java
+++ PHPTravels/src/main/java/com/qa/travels/base/TestBase.java
@@ -1,10 +1,38 @@
 package com.qa.travels.base;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TestBase {
 	
+	public WebDriver driver;
+	public Properties prop;
+	
 	public TestBase() {
-		System.out.println("test");
-		System.out.println("New Code added");
+		prop = new Properties();
+		try {
+			FileInputStream ip = new FileInputStream("/PHPTravels/src/main/java/com/qa/travels/config/config.properties");
+			prop.load(ip);
+		}catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void initialize() {
+		System.setProperty("webdriver.chrome.driver","/PHPTravels/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 
 }
